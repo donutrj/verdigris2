e751f84e163d37a98e88f1474cd8976c1ab8a2cb
donutrj <00rolland00@gmail.com>
2024-07-09 17:42:13 -0700

    Quartz sync: Jul 9, 2024, 5:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -28,14 +28,14 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#faf8f8",                       //background
+          lightgray: "#e5e5e5",                   //borders
+          gray: "#b8b8b8",                        //graph links, heavy borders
+          darkgray: "#4e4e4e",                    //body text
+          dark: 'hsla(33, 54%, 17%, 1)',                        //header text & icons
+          secondary: "#284b63",                   //link color
+          tertiary: "#84a59d",                    //hover state
+          highlight: "rgba(143, 159, 169, 0.15)", //markdown highlighted text background
         },
         darkMode: {
           light: "#161618",
